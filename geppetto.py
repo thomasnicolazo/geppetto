6466f6877e10f7bcf89300de2ad8e6b6ef0ac6c8
Thomas Nicolazo <thomas.nicolazo@gmail.com>
2026-01-18 19:44:00 +0100

    initial commit

---
+++ geppetto.py
@@ -0,0 +1,31 @@
+import paramiko
+import logging
+import os
+import argparse
+import yaml
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#client.load_system_host_keys()
+# private_key_path = '/path/to/private_key'
+# private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+# ssh.connect('hostname', username='username', pkey=private_key)
+client.connect('scp721', username='thomas', password='')
+stdin, stdout, stderr = client.exec_command('ls')
+print(stdout.read().decode())
+client.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='geppetto.py', usage='%(prog)s [options]')
+    parser.add_argument( '-c', '--config-file', help='path to the config file ',default="config.yaml")
+    parser.add_argument('-t', '--test', help='run test script for scripts in config file', action='store_true')
+    args = parser.parse_args()
+    logger = logging.getLogger(__name__)
+    if not os.path.exists(args.config_file):
+        raise FileNotFoundError(args.config_file)
+    with open(args.config_file, 'r') as conf_file:
+        try:
+            yaml_cfg = yaml.safe_load(conf_file)
+        except yaml.YAMLError as exc:
+            logger.error(exc)
