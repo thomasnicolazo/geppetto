736d397c9f2e7b464eed020c6b6acc02fac53995
Thomas Nicolazo <thomas.nicolazo@gmail.com>
2026-01-18 20:23:12 +0100

    add logger formater module to format logger

--- geppetto.py
+++ geppetto.py
@@ -1,8 +1,11 @@
 import paramiko
 import logging
 import os
+import sys
 import argparse
 import yaml
+
+import logger.logger_formater as formater
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -10,7 +13,7 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # private_key_path = '/path/to/private_key'
 # private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
 # ssh.connect('hostname', username='username', pkey=private_key)
-client.connect('scp721', username='thomas', password='')
+client.connect('scp721', username='thomas', password='test')
 stdin, stdout, stderr = client.exec_command('ls')
 print(stdout.read().decode())
 client.close()
@@ -22,10 +25,17 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--test', help='run test script for scripts in config file', action='store_true')
     args = parser.parse_args()
     logger = logging.getLogger(__name__)
+    #logging.basicConfig(level=logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formater.CustomFormatter())
+    logger.addHandler(handler)
     if not os.path.exists(args.config_file):
         raise FileNotFoundError(args.config_file)
+        exit(1)
     with open(args.config_file, 'r') as conf_file:
         try:
             yaml_cfg = yaml.safe_load(conf_file)
         except yaml.YAMLError as exc:
             logger.error(exc)
+
+    machines = yaml_cfg['machines']
---
+++ logger/logger_formater.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+    grey = "\033[38;21m"
+    yellow = "\033[33;21m"
+    red = "\033[91m"
+    bold_red = "\033[31;1m"
+    reset = "\033[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
